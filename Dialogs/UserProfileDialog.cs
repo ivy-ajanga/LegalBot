f5391b20b035f429ae3ac133c6868a6ecd37909d
unknown <ajangaivy@gmail.com>
2020-08-20 13:13:11 +0300

    first commit

---
+++ Dialogs/UserProfileDialog.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class UserProfileDialog : ComponentDialog
+    {
+        private readonly IStatePropertyAccessor<UserProfile> _userProfileAccessor;
+
+        public UserProfileDialog(UserState userState)
+            : base(nameof(UserProfileDialog))
+        {
+            _userProfileAccessor = userState.CreateProperty<UserProfile>("UserProfile");
+
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {
+                LanguageStepAsync,
+                NameStepAsync,
+                CountyStepAsync,
+                SubcountyStepAsync,
+                WardStepAsync,              
+                SummaryStepAsync,
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+         
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private static async Task<DialogTurnResult> LanguageStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is a Prompt Dialog.
+            // Running a prompt here means the next WaterfallStep will be run when the user's response is received.
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Hello,Welcome to our service.Please choose your preferred Langauage"),
+                    Choices = ChoiceFactory.ToChoices(new List<string> {"English", "Kisawahili"}),
+                }, cancellationToken);
+        }
+
+        private static async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["name"] = ((FoundChoice)stepContext.Result).Value;
+
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Please enter your name.") }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> CountyStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+          
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+               new PromptOptions
+               {
+                   Prompt = MessageFactory.Text("Which county do you live in?"),
+                   Choices = ChoiceFactory.ToChoices(new List<string> { "Nairobi", "Kisumu" ,"Trans-Nzoia"}),
+               }, cancellationToken);
+
+        }
+        private async Task<DialogTurnResult> SubcountyStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+               new PromptOptions
+               {
+                   Prompt = MessageFactory.Text("Which sub - county do you live in?"),
+                   Choices = ChoiceFactory.ToChoices(new List<string> { "Nairobi", "Kisumu", "Trans-Nzoia" }),
+               }, cancellationToken);
+        }
+
+        
+        private async Task<DialogTurnResult> WardStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+               new PromptOptions
+               {
+                   Prompt = MessageFactory.Text("Which ward do you live in?"),
+                   Choices = ChoiceFactory.ToChoices(new List<string> { "Nairobi", "Kisumu", "Trans-Nzoia" }),
+               }, cancellationToken);
+
+        }
+
+
+        private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+        
+             // Get the current profile object from user state.
+             var userProfile = await _userProfileAccessor.GetAsync(stepContext.Context, () => new UserProfile(), cancellationToken);
+         
+            userProfile.Name = (string)stepContext.Values["name"];            
+
+            var msg = $"Your name is {userProfile.Name}";
+             
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text(msg), cancellationToken);         
+         
+
+            // WaterfallStep always finishes with the end of the Waterfall or with another dialog; here it is the end.
+            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+            
+        }
+    
+    }
+}
---
+++ UserProfile.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+    /// <summary>
+    /// This is our application state. Just a regular serializable .NET class.
+    /// </summary>
+    public class UserProfile
+    {
+        public string Language { get; set; }
+
+        public string Name { get; set; }
+
+        public string County { get; set; }
+
+        public string Subcounty { get; set; }
+       
+
+        public string Ward { get; set; }
+    }
+}
